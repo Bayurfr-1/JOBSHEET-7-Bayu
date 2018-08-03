7106b2b322cc4abc3301f8081b6d9cd6a4160f16
win 7 <win 7@win7-PC.mshome.net>
2018-08-03 11:11:06 +0700

    Coba IF...ELSE IF...ELSE..

--- src/jobsheet/pkg7/Latihan1.java
+++ src/jobsheet/pkg7/Latihan1.java
@@ -37,7 +37,34 @@ public class Latihan1 {
                System.out.println("isi variable a :"+a);
                System.out.println("nilai kurang dari 5");
            } else System.out.println("nilai a lebih dari atau sama dengan 5");
+           System.out.println("\nIF..ELSE IF...ELSE");
+           if(a<5){
+               System.out.println("nilai a kurang dari 5");
+           }else if(a==5) {
+               System.out.println("nilai a sama dengan 5");
+           }else
+               
+               System.out.println("nilai lebih dari 5");
+           
+            System.out.println("\nIF..ELSE IF...ELSE 2");
+            if(a<2){
+                System.out.println("nilai a kurang dari 2");
+            }else if(a<4) {
+                System.out.println("nilai a kurang dari 4");
+            } else if(a<6){
+                System.out.println("nilai a kurang dari 6 ");
+            }else if (a==6){
+                System.out.println("Nilai a sama dengan 6");
+            }else {
+                System.out.println("nilai a lebih dari 6");
+            }
+                
+                
+            }
+            
+            
+           }
         } 
     }
     
-}
+
